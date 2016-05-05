18410c499dcb0ff924c55a5d65bddd75e02bd8b3
leo <lidongjie2008@hotmail.com>
2016-05-05 22:37:53 +0800

    last

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,8 @@ def post_list(request):
     posts = Post.objects.all().order_by('publish_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
